3a2d32b3e32a3d507924679f1e4ac0dee73d2ad4
Dima Szamozvancev <ds709@cam.ac.uk>
2016-11-04 10:39:50 +0000

    Change duration model, start implementing intervals
    Durations are now simple type-level naturals.
    Note is a data constructor of type Unit, which also includes Rests.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeInType, GADTs, TypeFamilies, ConstraintKinds, RankNTypes, TypeOperators #-}
+{-# LANGUAGE TypeInType, GADTs, TypeFamilies, TypeOperators, TypeApplications, UndecidableInstances #-}
 
 module Lib
      where
@@ -6,13 +6,14 @@ module Lib
 import GHC.TypeLits
 import GHC.Exts
 import Data.Kind hiding (Constraint)
+import Data.Proxy
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
 data PitchClass = C | D | E | F | G | A | B deriving (Eq, Show, Enum, Ord)
 
-data Accidental = Nat | Flat | Sharp deriving (Eq, Show, Enum, Ord)
+data Accidental = Natural | Flat | Sharp deriving (Eq, Show, Enum, Ord)
 
 data Oct = Oct_1 | Oct0 | Oct1 | Oct2 | Oct3 | Oct4 | Oct5 | Oct6 | Oct7 | Oct8 deriving (Eq, Show, Ord, Enum, Bounded)
 
@@ -26,17 +27,57 @@ type Sixteenth = 2
 type ThirtySecond = 1
 
 
-data Duration :: Nat -> Type where
-    Dur :: Duration n
-    (:^:) :: Duration d1 -> Duration d2 -> Duration (d1 + d2)
+type Duration = Nat
+-- Proxy
+data Dur (a :: Duration) = Dur
+
+-- type family HalfOf (d :: Duration) :: Duration where
+--     HalfOf Sixteenth = ThirtySecond
+--     HalfOf Eighth = Sixteenth
+--     HalfOf Quarter = Eighth
+--     HalfOf Half = Quarter
+--     HalfOf Whole = Half
+--
+-- type family Dotted (u :: Unit d) :: Unit e where
+--     -- Dotted ThirtySecond = TypeError (Text "Can't create a dotted thirty-second note.")
+--     Dotted (Note p dur) = Note p (Dur :: Dur (dur + HalfOf(dur)))
+--     Dotted Eighth = Eighth + Sixteenth
+--     Dotted Quarter = Quarter + Eighth
+--     Dotted Half = Half + Quarter
+
+
 
 data IntervalSize = Unison | Second | Third | Fourth | Fifth | Sixth | Seventh | Octave deriving (Eq, Show, Ord)
 
-data Pitch (pc :: PitchClass) (a :: Accidental) (o :: Oct) where
-    Pitch :: Pitch pc a o
+data IntervalClass = Maj | Perf | Min | Aug | Dim deriving (Eq, Show)
 
-data Note :: Pitch pc a o -> Duration d -> Type where
-    Note :: Note p d
+data Interval = Interval IntervalClass IntervalSize deriving (Eq, Show)
+
+-- type family Shrink (i :: Interval) :: Interval where
+--     Shrink ('Interval Perf Unison) = 'Interval Perf Unison
+--     Shrink ('Interval Perf )
+
+data Pitch where
+    Pitch :: PitchClass -> Accidental -> Oct -> Pitch
+
+type family MakeInterval (p1 :: Pitch) (p2 :: Pitch) :: Interval where
+    MakeInterval ('Pitch C Natural o1) ('Pitch C Natural o1) = 'Interval Perf Unison
+    MakeInterval ('Pitch C Natural o1) ('Pitch D Natural o1) = 'Interval Maj Second
+    MakeInterval ('Pitch C Natural o1) ('Pitch E Natural o1) = 'Interval Maj Third
+    MakeInterval ('Pitch C Natural o1) ('Pitch F Natural o1) = 'Interval Perf Fourth
+    MakeInterval ('Pitch C Natural o1) ('Pitch G Natural o1) = 'Interval Perf Fifth
+    MakeInterval ('Pitch C Natural o1) ('Pitch A Natural o1) = 'Interval Maj Sixth
+    MakeInterval ('Pitch C Natural o1) ('Pitch B Natural o1) = 'Interval Maj Seventh
+    MakeInterval ('Pitch C Natural o1) ('Pitch C Natural o2) = 'Interval Perf Octave
+    MakeInterval ('Pitch C Natural o1) ('Pitch pc2 Sharp o2) = 'Interval Perf Octave
+
+x :: Proxy (MakeInterval ('Pitch C Natural Oct3) ('Pitch C Natural Oct4))
+x = undefined
+
+
+data Unit :: Duration -> Type where
+    Note :: Pitch -> Dur d -> Unit d
+    Rest :: Dur d -> Unit d
 
 -- type family ValidDuration (d :: Duration) :: Bool where
 --     ValidDuration Whole = True
@@ -49,17 +90,12 @@ data Note :: Pitch pc a o -> Duration d -> Type where
 -- type family Ensure (b :: Bool) :: Constraint where
 --     Ensure v = v ~ True
 --
--- x :: Note p (Half (Half (Half (Half (Half Whole)))))
--- x = Note
-
-
--- data Music = Prim (forall p d . Note p d) | Music :-: Music | Music :|: Music
 
 data Music :: Nat -> Type where
-    Prim :: Note p (d :: Duration n) -> Music n
+    Prim :: Unit d -> Music d
     (:-:) :: Music d1 -> Music d2 -> Music (d1 + d2)
+    (:|:) :: d1 ~ d2 => Music d1 -> Music d2 -> Music d1
 
--- type family (d1 :: Duration) +~ (d2 :: Duration)
 
--- type family SumLength (pl1 :: PieceLength) (pl2 :: PieceLength) :: PieceLength where
---     SumLength (PL n )
+m :: Music Half
+m = Prim (Rest (Dur @Half)) :|: Prim (Rest (Dur @Half))
