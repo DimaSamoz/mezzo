386ad19ae0223f3bc72cff420d958130ce3c6187
Dima Szamozvancev <ds709@cam.ac.uk>
2016-11-06 12:04:45 +0000

    Add some auxiliary type families and modifications

--- src/Lib.hs
+++ src/Lib.hs
@@ -53,12 +53,70 @@ data IntervalClass = Maj | Perf | Min | Aug | Dim deriving (Eq, Show)
 
 data Interval = Interval IntervalClass IntervalSize deriving (Eq, Show)
 
--- type family Shrink (i :: Interval) :: Interval where
---     Shrink ('Interval Perf Unison) = 'Interval Perf Unison
---     Shrink ('Interval Perf )
+type family Shrink (i :: Interval) :: Interval where
+    Shrink ('Interval Perf Unison) = 'Interval Perf Unison
+    Shrink ('Interval Perf is) = 'Interval Dim is
+    Shrink ('Interval Min is) = 'Interval Dim is
+    Shrink ('Interval Maj is) = 'Interval Min is
+
+type family Expand (i :: Interval) :: Interval where
+    Expand ('Interval Perf Octave) = 'Interval Perf Octave
+    Expand ('Interval Perf is) = 'Interval Aug is
+    Expand ('Interval Maj is) = 'Interval Aug is
+    Expand ('Interval Min is) = 'Interval Maj is
+
+type family OctSucc (o :: Oct) :: Oct where
+    OctSucc Oct_1 = Oct0
+    OctSucc Oct0 = Oct1
+    OctSucc Oct1 = Oct2
+    OctSucc Oct2 = Oct3
+    OctSucc Oct3 = Oct4
+    OctSucc Oct4 = Oct5
+    OctSucc Oct5 = Oct6
+    OctSucc Oct6 = Oct7
+    OctSucc Oct7 = Oct8
+    OctSucc Oct8 = TypeError (Text "Octave too high.")
+
+type family OctPred (o :: Oct) :: Oct where
+    OctPred Oct_1 = TypeError (Text "Octave too low.")
+    OctPred Oct0 = Oct_1
+    OctPred Oct1 = Oct0
+    OctPred Oct2 = Oct1
+    OctPred Oct3 = Oct2
+    OctPred Oct4 = Oct3
+    OctPred Oct5 = Oct4
+    OctPred Oct6 = Oct5
+    OctPred Oct7 = Oct6
+    OctPred Oct8 = Oct7
+
+type family ClassSucc (c :: PitchClass) :: PitchClass where
+    ClassSucc C = D
+    ClassSucc D = E
+    ClassSucc E = F
+    ClassSucc F = G
+    ClassSucc G = A
+    ClassSucc A = B
+    ClassSucc B = C
+
+type family ClassPred (c :: PitchClass) :: PitchClass where
+    ClassPred C = B
+    ClassPred D = C
+    ClassPred E = D
+    ClassPred F = E
+    ClassPred G = F
+    ClassPred A = G
+    ClassPred B = A
+
 
 data Pitch where
     Pitch :: PitchClass -> Accidental -> Oct -> Pitch
+
+type family HalfStepUp (p :: Pitch) :: Pitch where
+    HalfStepUp ('Pitch B acc o) = 'Pitch C acc (OctSucc o)
+    HalfStepUp ('Pitch E acc o) = 'Pitch F acc o
+    HalfStepUp ('Pitch pc Flat o) = 'Pitch pc Natural o
+    HalfStepUp ('Pitch pc Natural o) = 'Pitch pc Sharp o
+    HalfStepUp ('Pitch pc Sharp o) = 'Pitch (ClassSucc pc) Sharp o
 
 type family MakeInterval (p1 :: Pitch) (p2 :: Pitch) :: Interval where
     MakeInterval ('Pitch C Natural o1) ('Pitch C Natural o1) = 'Interval Perf Unison
@@ -69,14 +127,31 @@ type family MakeInterval (p1 :: Pitch) (p2 :: Pitch) :: Interval where
     MakeInterval ('Pitch C Natural o1) ('Pitch A Natural o1) = 'Interval Maj Sixth
     MakeInterval ('Pitch C Natural o1) ('Pitch B Natural o1) = 'Interval Maj Seventh
     MakeInterval ('Pitch C Natural o1) ('Pitch C Natural o2) = 'Interval Perf Octave
-    MakeInterval ('Pitch C Natural o1) ('Pitch pc2 Sharp o2) = 'Interval Perf Octave
+    MakeInterval ('Pitch C Natural o1) ('Pitch pc2 Sharp o2) =
+            Expand (MakeInterval ('Pitch C Natural o1) ('Pitch pc2 Natural o2))
+    MakeInterval ('Pitch C Natural o1) ('Pitch pc2 Flat o2) =
+            Shrink (MakeInterval ('Pitch C Natural o1) ('Pitch pc2 Natural o2))
 
-x :: Proxy (MakeInterval ('Pitch C Natural Oct3) ('Pitch C Natural Oct4))
+    MakeInterval _ _ = TypeError (Text "Invalid interval.")
+
+x :: Proxy (MakeInterval ('Pitch C Natural (OctSucc Oct8)) ('Pitch F Sharp Oct3))
 x = undefined
 
+y :: Proxy (OctSucc Oct8)
+y = undefined
+
+type family ValidPitch (p :: Pitch) :: Bool where
+    ValidPitch ('Pitch pc acc o) = TypeError (Text "Octave too high.")
+    -- ValidPitch _ = True
+
+-- z :: Unit 4
+-- z = Note (Proxy @('Pitch C Natural (OctSucc Oct8))) (Dur @Eighth)
+
+z :: Proxy (Note (Proxy ('Pitch C Natural Oct3)) (Dur Half))
+z = undefined
 
 data Unit :: Duration -> Type where
-    Note :: Pitch -> Dur d -> Unit d
+    Note :: Ensure (ValidPitch p) => Proxy p -> Dur d -> Unit d
     Rest :: Dur d -> Unit d
 
 -- type family ValidDuration (d :: Duration) :: Bool where
@@ -87,9 +162,9 @@ data Unit :: Duration -> Type where
 -- -- type family ToInterval (pc1 :: PitchClass) (pc2 :: PitchClass) :: Interval where
 -- --     ToInterval
 --
--- type family Ensure (b :: Bool) :: Constraint where
---     Ensure v = v ~ True
---
+type family Ensure (b :: Bool) :: Constraint where
+    Ensure v = v ~ True
+
 
 data Music :: Nat -> Type where
     Prim :: Unit d -> Music d
@@ -98,4 +173,4 @@ data Music :: Nat -> Type where
 
 
 m :: Music Half
-m = Prim (Rest (Dur @Half)) :|: Prim (Rest (Dur @Half))
+m = Prim (Note (Proxy @('Pitch C Natural Oct3)) (Dur @Half)) :|: Prim (Rest (Dur @Half))
