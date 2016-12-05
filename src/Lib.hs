606bac4aafbfb220cc5bd085e6a8747c08b76f5a
Dima Szamozvancev <ds709@cam.ac.uk>
2016-12-05 11:25:44 +0000

    Various additions
    – Harmony and melody interval checking
    – Revise Music type
    – Revise Note and Rest
    – Convert notes to vectors

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE TypeInType, GADTs, TypeFamilies, TypeOperators, FlexibleContexts, MultiParamTypeClasses,
-    TypeApplications, UndecidableInstances, FlexibleInstances, RankNTypes, ConstraintKinds #-}
+    TypeApplications, UndecidableInstances, FlexibleInstances, RankNTypes, ConstraintKinds, PartialTypeSignatures #-}
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.Normalise #-}
+
 
 module Lib
      where
@@ -19,9 +20,19 @@ data Accidental = Natural | Flat | Sharp deriving (Eq, Show, Enum, Ord)
 
 data Oct = Oct_1 | Oct0 | Oct1 | Oct2 | Oct3 | Oct4 | Oct5 | Oct6 | Oct7 | Oct8 deriving (Eq, Show, Ord, Enum, Bounded)
 
--- data Oct = Nat
+type Duration = Nat
 
--- data Duration = Duration :^: Duration | Whole | Half Duration deriving (Eq, Show)
+data PCS (pc :: PitchClass) where
+    PCS :: PCS pc
+
+data AccS (acc :: Accidental) where
+    AccS :: AccS acc
+
+data OctS (oct :: Oct) where
+    OctS :: OctS oct
+
+data Dur (a :: Duration) where
+    Dur :: Dur a
 
 type Whole = 32
 type Half = 16
@@ -31,30 +42,35 @@ type Sixteenth = 2
 type ThirtySecond = 1
 
 
-type Duration = Nat
--- Proxy
-data Dur (a :: Duration) where
-    Dur :: Dur a
 
-data Pitch :: PitchClass -> Accidental -> Oct -> Type where
-    Pitch :: Pitch pc acc o
+-- data Pitch :: PitchClass -> Accidental -> Oct -> Type where
+--     Pitch :: Pitch pc acc o
 
-data Note :: PitchClass -> Accidental -> Oct -> Duration -> Type where
-    Note :: Pitch pc acc o -> Dur d -> Note pc acc o d
+data PitchType where
+    Pitch :: PitchClass -> Accidental -> Oct -> PitchType
+    Silence :: PitchType
 
-data Rest :: Duration -> Type where
-    Rest :: Dur d -> Rest d
+data PitchS (p :: PitchType) where
+    PitchS :: PitchS p
+
+-- data Note :: PitchClass -> Accidental -> Oct -> Duration -> Type where
+--     Note :: Pitch pc acc o -> Dur d -> Note pc acc o d
+--
+-- data Rest :: Duration -> Type where
+--     Rest :: Dur d -> Rest d
 
 -- data Unit :: Duration -> Type where
 --     NoteU :: Note p acc o d -> Unit d
 --     RestU :: Rest d -> Unit d
 
-n :: Note C Natural Oct3 (Dotted Half)
-n = Note Pitch Dur
+-- n :: Music _
+n = Note (PCS @C) (AccS @Natural) (OctS @Oct3) (Dur @Sixteenth)
 
-class Unit u
-instance Unit (Note pc acc oct dur)
-instance Unit (Rest dur)
+r = Rest (Dur @Eighth)
+
+-- class Unit u
+-- instance Unit (Note pc acc oct dur)
+-- instance Unit (Rest dur)
 
 type family HalfOf (d :: Duration) :: Duration where
     HalfOf ThirtySecond = TypeError (Text "Note duration can't be smaller than 1/32")
@@ -70,6 +86,7 @@ type family Dotted (d :: Duration) :: Duration where
 data Vec :: Type -> Nat -> Type where
     Nil :: Vec a 0
     (:-) :: a -> Vec a (n - 1) -> Vec a n
+infixr 5 :-
 
 type family (x :: Vec a n) ++ (y :: Vec a m) :: Vec a (n + m) where
     Nil ++ y = y
@@ -83,6 +100,15 @@ type family (a :: Matrix t p q) +-+ (b :: Matrix t p r) :: Matrix t p (q + r) wh
 
 type family (a :: Matrix t p r) +|+ (b :: Matrix t q r) :: Matrix t (p + q) r where
     m1 +|+ m2 = m1 ++ m2
+
+type family Transpose (a :: Matrix t p q) :: Matrix t q p where
+    Transpose Nil = Nil
+    Transpose (Nil :- Nil) = Nil
+    Transpose ((x :- xs) :- Nil) = (x :- Nil) :- (Transpose (xs :- Nil))
+    Transpose (row :- rows) = (Transpose (row :- Nil)) +-+ Transpose rows
+
+matT :: Proxy (Transpose ((1 :- 2 :- Nil) :- (3 :- 4 :- Nil) :- Nil))
+matT = undefined
 
 -- data UnitP (u :: Unit d) = UnitP
 
@@ -106,99 +132,132 @@ test = Proxy @60
 test2 :: Proxy (NatToPitch 61)
 test2 = Proxy @(Pitch C Sharp Oct4)
 
+type family Repeat (a :: t) (n :: Nat) :: Vec t n where
+    Repeat x 0 = Nil
+    Repeat x n = x :- (Repeat x (n-1))
+
+type family NoteToVec (p :: PitchType) (d :: Duration) :: Vec PitchType d where
+    NoteToVec pitch dur = Repeat pitch dur
+
+type family RestToVec (d :: Duration) :: Vec PitchType d where
+    RestToVec dur = Repeat Silence dur
+
 data Music :: forall u p q. Matrix u p q -> Type where
-    Prim :: Unit u => u -> Music ((u :- Nil) :- Nil)
-    (:|:) :: Music m1 -> Music m2 -> Music (m1 +|+ m2)
-    (:-:) :: AllValidLeaps m1 m2 => Music m1 -> Music m2 -> Music (m1 +-+ m2)
+    Note :: PCS pc -> AccS acc -> OctS oct -> Dur d -> Music (NoteToVec (Pitch pc acc oct) d :- Nil)
+    Rest :: Dur d -> Music (RestToVec d :- Nil)
+    (:|:) :: ValidParComp m1 m2 => Music m1 -> Music m2 -> Music (m1 +|+ m2)
+    (:-:) :: ValidSeqComp m1 m2 => Music m1 -> Music m2 -> Music (m1 +-+ m2)
 
--- type family ValidLeap (v1 :: Vec a n) (v2 :: Vec a m) :: Constraint where
---     ValidLeap Nil _ = Valid
---     ValidLeap _ Nil = Valid
---     ValidLeap (v1 :- Nil) (v2 :- vs2) = ValidNoteLeap v1 v2
---     ValidLeap (_ :- vs1) vs2 = ValidLeap vs1 vs2
+type Composition = forall m. Music m
+data Comp = forall m. Comp (Music m)
 
-class ValidLeap a b
-instance ValidLeap Nil a
-instance ValidLeap a Nil
-instance ValidNoteLeap v1 v2 => ValidLeap (v1 :- Nil) (v2 :- vs2)
-instance {-# OVERLAPPABLE #-}  ValidLeap vs1 vs2 => ValidLeap (v :- vs1) vs2
+type ValidParComp m1 m2 = (AllSatisfy ValidIntervals (Transpose (m1 +|+ m2)))
+
+type ValidSeqComp m1 m2 = (AllValidLeaps m1 m2)
+
+
+class ValidMelInterval i
+instance {-# OVERLAPPING #-}    ValidMelInterval (Interval Aug Unison)
+instance {-# OVERLAPS #-}       TypeError (Text "Augmented melodic intervals are not permitted.") => ValidMelInterval (Interval Aug a)
+instance {-# OVERLAPS #-}       TypeError (Text "Augmented melodic intervals are not permitted.") => ValidMelInterval (Interval Dim a)
+instance {-# OVERLAPPING #-}    TypeError (Text "Seventh intervals are not permitted in melody.") => ValidMelInterval (Interval a Seventh)
+instance {-# OVERLAPPABLE #-}   ValidMelInterval i
 
 class ValidNoteLeap n1 n2
-instance ValidNoteLeap (Rest d) a
-instance ValidNoteLeap a (Rest d)
-instance ValidMelInterval (MakeInterval n1 n2) => ValidNoteLeap n1 n2
+instance {-# OVERLAPPING #-} ValidNoteLeap (Silence) (Pitch pc acc oct)
+instance {-# OVERLAPPING #-} ValidNoteLeap (Pitch pc acc oct) (Silence)
+instance {-# OVERLAPPABLE #-} ValidMelInterval (MakeInterval a b) => ValidNoteLeap a b
 
+class ValidLeap (a :: Vec t n) (b :: Vec t m)
+instance {-# OVERLAPPING #-} ValidLeap Nil a
+instance {-# OVERLAPPING #-} ValidLeap a Nil
+instance {-# OVERLAPS #-} ValidNoteLeap v1 v2 => ValidLeap (v1 :- Nil) (v2 :- vs2)
+instance {-# OVERLAPPABLE #-} ValidLeap vs1 vs2 => ValidLeap (v :- vs1) vs2
+
+class AllValidLeaps (m1 :: Matrix t p q) (m2 :: Matrix t p r)
+instance {-# OVERLAPPING #-} AllValidLeaps Nil Nil
+instance {-# OVERLAPPABLE #-} (ValidLeap row1 row2, AllValidLeaps rest1 rest2) => AllValidLeaps (row1 :- rest1) (row2 :- rest2)
+
+
+
+
+class ValidHarmInterval i
+instance {-# OVERLAPPING #-} TypeError (Text "Can't have minor seconds in chords.") => ValidHarmInterval (Interval Aug Unison)
+instance {-# OVERLAPPING #-} TypeError (Text "Can't have minor seconds in chords.") => ValidHarmInterval (Interval Min Second)
+instance {-# OVERLAPPING #-} TypeError (Text "Can't have major sevenths in chords.") => ValidHarmInterval (Interval Maj Seventh)
+instance {-# OVERLAPPING #-} TypeError (Text "Can't have major sevenths in chords.") => ValidHarmInterval (Interval Dim Octave)
+instance {-# OVERLAPPING #-} TypeError (Text "Can't have augmented octaves in chords.") => ValidHarmInterval (Interval Aug Octave)
+instance {-# OVERLAPPABLE #-} ValidHarmInterval i
+-- instance (Interval Aug Unison ~ i, TypeError (Text "Can't have minor seconds in chords.")) => ValidHarmInterval i
+
+class ValidNoteInterval n1 n2
+instance {-# OVERLAPPING #-} ValidNoteInterval (Silence) (Silence)
+instance {-# OVERLAPPING #-} ValidNoteInterval (Pitch pc acc oct) (Silence)
+instance {-# OVERLAPPING #-} ValidNoteInterval (Silence) (Pitch pc acc oct)
+instance {-# OVERLAPPABLE #-} ValidHarmInterval (MakeInterval a b) => ValidNoteInterval a b
+
+type family AllPairsSatisfy (c :: a -> a -> Constraint) (e :: a) (xs :: Vec a n) :: Constraint where
+    AllPairsSatisfy c e xs = AllSatisfy (c e) xs
+
+type family AllSatisfy (c :: a -> Constraint) (xs :: Vec a n) :: Constraint where
+    AllSatisfy c Nil = Valid
+    AllSatisfy c (x :- xs) = ((c x), AllSatisfy c xs)
+
+class ValidIntervals (m :: Vec t n)
+instance {-# OVERLAPPING #-} ValidIntervals Nil
+instance {-# OVERLAPPING #-} ValidIntervals (n :- Nil)
+instance {-# OVERLAPPING #-} ValidNoteInterval n1 n2 => ValidIntervals (n1 :- n2 :- Nil)
+instance {-# OVERLAPPABLE #-} (AllPairsSatisfy ValidNoteInterval n ns, ValidIntervals ns) => ValidIntervals (n :- ns)
+
+
+
+type family Head (v :: Vec a n) :: a where
+    Head (x :- xs) = x
+
+type family Tail (v :: Vec a n) :: Vec a (n - 1) where
+    Tail (x :- xs) = xs
 
 -- type family ValidNoteLeap n1 n2 :: Constraint where
 --     ValidNoteLeap (Rest d) _ = Valid
 --     ValidNoteLeap _ (Rest d) = Valid
 --     ValidNoteLeap n1 n2 = ValidMelInterval (MakeInterval n1 n2)
 
-class AllValidLeaps m1 m2
-instance AllValidLeaps Nil Nil
-instance (ValidLeap row1 row2, AllValidLeaps rest1 rest2) => AllValidLeaps (row1 :- rest1) (row2 :- rest2)
-
 -- type family AllValidLeaps (m1 :: Matrix a p r) (m2 :: Matrix a p q) :: Bool where
 --     AllValidLeaps Nil Nil = True
 --     AllValidLeaps (row1 :- rest1) (row2 :- rest2) = ValidLeap row1 row2 `And` AllValidLeaps rest1 rest2
 
-type family And (b1 :: Bool) (b2 :: Bool) :: Bool where
-    And b1 b2 = If (b1) (b2) False
+type family (b1 :: Bool) .&&. (b2 :: Bool) :: Bool where
+    b1 .&&. b2 = If (b1) (b2) False
+infixl 3 .&&.
 
-type family Or (b1 :: Bool) (b2 :: Bool) :: Bool where
-    Or False False = False
-    Or _ _ = True
+type family (b1 :: Bool) .||. (b2 :: Bool) :: Bool where
+    False .||. False = False
+    _ .||.  _ = True
+infixl 3 .||.
 
-class ValidMelInterval i
-instance TypeError (Text "Augmented melodic intervals are not permitted.") => ValidMelInterval (Interval Aug a)
-instance TypeError (Text "Augmented melodic intervals are not permitted.") => ValidMelInterval (Interval Dim a)
-instance TypeError (Text "Seventh intervals are not permitted in melody.") => ValidMelInterval (Interval a Seventh)
-instance ValidMelInterval (Interval a b)
-
--- type family ValidMelInterval i :: Constraint where
---     ValidMelInterval (Interval Aug _) = TypeError (Text "Augmented melodic intervals are not permitted.")
---     ValidMelInterval (Interval Dim _) = TypeError (Text "Diminished melodic intervals are not permitted.")
---     ValidMelInterval (Interval _ Seventh) = TypeError (Text "Seventh intervals are not permitted in melody.")
---     ValidMelInterval (Interval _ _) = Valid
-
-invLeap :: Proxy (ValidLeap (Note C Natural Oct3 Half :- Nil) (Note G Flat Oct3 Half :- Nil))
-invLeap = undefined
-
-type family OrdPair p1 p2 :: (Nat, Nat) where
-    OrdPair p1 p2 =
-        If  (PitchToNat p1 <=? PitchToNat p2)
-            '((PitchToNat p1), (PitchToNat p2))
-            '((PitchToNat p2), (PitchToNat p1))
-
--- type Leap n1 n2 = MakeInterval
-
-primR :: Music (((Rest Half) :- Nil) :- Nil)
-primR = Prim (Rest (Dur @Half))
-
-primN :: Music ((Note F Sharp Oct3 Half :- Nil) :- Nil)
-primN = Prim (Note (Pitch @F @Sharp @Oct3) (Dur @Half))
-
-primMel :: Music ((Rest Half :- Nil) :- (Nil ++ ((Note F Sharp Oct3 Half :- Nil) :- Nil)))
-primMel = primR :|: primN
-
-bbt :: Proxy (MakeInterval (Pitch F Natural Oct3) (Pitch B Natural Oct3))
-bbt = undefined
-
-bb =
-    (Prim (Note (Pitch @C @Natural @Oct3) (Dur @Eighth))) :-:
-    (Prim (Note (Pitch @E @Natural @Oct3) (Dur @Eighth)))
-
-bt = ( bb) :|: bb
+bb :: Comp
+bb = Comp $
+    ((Note (PCS @G) (AccS @Natural) (OctS @Oct3) (Dur @Eighth)) :|:
+    (Note (PCS @B) (AccS @Natural) (OctS @Oct3) (Dur @Eighth)) :|:
+    (Note (PCS @D) (AccS @Natural) (OctS @Oct4) (Dur @Eighth)) :|:
+    (Note (PCS @F) (AccS @Natural) (OctS @Oct4) (Dur @Eighth)))
+    :-:
+    ((Note (PCS @G) (AccS @Natural) (OctS @Oct3) (Dur @Eighth)) :|:
+    (Note (PCS @C) (AccS @Natural) (OctS @Oct4) (Dur @Eighth)) :|:
+    (Note (PCS @E) (AccS @Natural) (OctS @Oct5) (Dur @Eighth)) :|:
+    (Rest (Dur @Sixteenth) :-: Rest (Dur @Sixteenth)))
+-- bt = ( bb) :|: bb
 
 
 data IntervalSize = Unison | Second | Third | Fourth | Fifth | Sixth | Seventh | Octave deriving (Eq, Show, Ord)
 
 data IntervalClass = Maj | Perf | Min | Aug | Dim deriving (Eq, Show)
 
-data Interval :: IntervalClass -> IntervalSize -> Type where
-    Interval :: Interval ic is
+data IntervalType where
+    Interval :: IntervalClass -> IntervalSize -> IntervalType
+    Compound :: IntervalType
 
-type family Shrink i where
+type family Shrink (i :: IntervalType) :: IntervalType where
     Shrink (Interval Perf Unison) = TypeError (Text "Can't diminish unisons.")
     Shrink (Interval Perf is) = Interval Dim is
     Shrink (Interval Min is) = Interval Dim is
@@ -213,8 +272,9 @@ type family Shrink i where
     Shrink (Interval Dim Fifth) = Interval Perf Fourth
     Shrink (Interval Dim Sixth) = Interval Dim Fifth
     Shrink (Interval Dim is) = Interval Min (IntSizePred is)
+    Shrink (Compound) = Compound
 
-type family Expand i where
+type family Expand (i :: IntervalType) :: IntervalType where
     Expand (Interval Perf Octave) = Interval Aug Octave
     Expand (Interval Perf is) = Interval Aug is
     Expand (Interval Maj is) = Interval Aug is
@@ -227,8 +287,9 @@ type family Expand i where
     Expand (Interval Aug Third) = Interval Aug Fourth
     Expand (Interval Aug Fourth) = Interval Perf Fifth
     Expand (Interval Aug Seventh) = Interval Aug Octave
-    Expand (Interval Aug Octave) = TypeError (Text "Compound intervals are not supported.")
+    Expand (Interval Aug Octave) = Compound
     Expand (Interval Aug is) = Interval Maj (IntSizeSucc is)
+    Expand (Compound) = Compound
 
 type family OctSucc (o :: Oct) :: Oct where
     OctSucc Oct_1 = Oct0
@@ -318,23 +379,27 @@ type family a .~. b :: Bool where
 infixl 5 .~.
 
 type family MakeInterval p1 p2 where
-    MakeInterval (Note pc1 acc1 oct1 dur1) (Note pc2 acc2 oct2 dur2) =
-        MakeInterval (Pitch pc1 acc1 oct1) (Pitch pc2 acc2 oct2)
+    -- MakeInterval (Note pc1 acc1 oct1 dur1) (Note pc2 acc2 oct2 dur2) =
+    --     MakeInterval (Pitch pc1 acc1 oct1) (Pitch pc2 acc2 oct2)
     MakeInterval p1 p2 =
         If  (PitchToNat p1 <=? PitchToNat p2)
             (MakeIntervalOrd p1 p2)
             (MakeIntervalOrd p2 p1)
 
-type family MakeIntervalOrd p1 p2 where
+type family MakeIntervalOrd p1 p2 :: IntervalType where
     MakeIntervalOrd p p = Interval Perf Unison
-    MakeIntervalOrd (Pitch C Natural o) (Pitch D Natural o) = Interval Maj Second
-    MakeIntervalOrd (Pitch C Natural o) (Pitch E Natural o) = Interval Maj Third
-    MakeIntervalOrd (Pitch C Natural o) (Pitch F Natural o) = Interval Perf Fourth
-    MakeIntervalOrd (Pitch C Natural o) (Pitch G Natural o) = Interval Perf Fifth
-    MakeIntervalOrd (Pitch C Natural o) (Pitch A Natural o) = Interval Maj Sixth
-    MakeIntervalOrd (Pitch C Natural o) (Pitch B Natural o) = Interval Maj Seventh
-    MakeIntervalOrd (Pitch C Natural o) (Pitch C Natural o2) =
-            If (OctSucc o .~. o2) (Interval Perf Octave) (TypeError (Text "Compound intervals are not supported."))
+    MakeIntervalOrd (Pitch C acc o) (Pitch D acc o) = Interval Maj Second
+    MakeIntervalOrd (Pitch C acc o) (Pitch E acc o) = Interval Maj Third
+    MakeIntervalOrd (Pitch C acc o) (Pitch F acc o) = Interval Perf Fourth
+    MakeIntervalOrd (Pitch C acc o) (Pitch G acc o) = Interval Perf Fifth
+    MakeIntervalOrd (Pitch C acc o) (Pitch A acc o) = Interval Maj Sixth
+    MakeIntervalOrd (Pitch C acc o) (Pitch B acc o) = Interval Maj Seventh
+    MakeIntervalOrd (Pitch C acc o) (Pitch C acc o2) =
+            If (OctSucc o .~. o2) (Interval Perf Octave) (Compound)
+    MakeIntervalOrd (Pitch C Natural o) (Pitch C Sharp o2) =
+            If (OctSucc o .~. o2) (Interval Aug Octave) (Compound)
+    MakeIntervalOrd (Pitch C Flat o) (Pitch C Natural o2) =
+            If (OctSucc o .~. o2) (Interval Aug Octave) (Compound)
     MakeIntervalOrd (Pitch C Natural o) (Pitch pc2 Sharp o) =
             Expand (MakeIntervalOrd (Pitch C Natural o) (Pitch pc2 Natural o))
     MakeIntervalOrd (Pitch C Natural o) (Pitch pc2 Flat o) =
@@ -343,51 +408,29 @@ type family MakeIntervalOrd p1 p2 where
             Expand (MakeIntervalOrd (Pitch C Natural o) (Pitch pc2 acc o))
     MakeIntervalOrd (Pitch C Sharp o) (Pitch pc2 acc o) =
             Shrink (MakeIntervalOrd (Pitch C Natural o) (Pitch pc2 acc o))
-    MakeIntervalOrd (Pitch pc1 acc1 o) (Pitch pc2 acc2 o) =
-            MakeIntervalOrd (Pitch (ClassPred pc1) acc1 o) (Pitch (ClassPred pc2) acc2 o)
     MakeIntervalOrd (Pitch pc1 acc1 o1) (Pitch pc2 acc2 o2) =
-            If  (OctSucc o1 .~. o2)
+            If  (o1 .~. o2 .||. OctSucc o1 .~. o2)
                 (MakeIntervalOrd (HalfStepDown (Pitch pc1 acc1 o1)) (HalfStepDown (Pitch pc2 acc2 o2)))
-                (TypeError (Text "Compound intervals are not supported."))
-    MakeIntervalOrd (Note pc1 acc1 o1 d1) (Note pc2 acc2 o2 d2) = MakeIntervalOrd (Pitch pc1 acc1 o1) (Pitch pc2 acc2 o2)
+                (Compound)
+    -- MakeIntervalOrd (Note pc1 acc1 o1 d1) (Note pc2 acc2 o2 d2) = MakeIntervalOrd (Pitch pc1 acc1 o1) (Pitch pc2 acc2 o2)
     MakeIntervalOrd _ _ = TypeError (Text "Invalid interval.")
 
-x :: MakeInterval (Pitch E Natural Oct3) (Pitch C Natural Oct_1)
+x :: Proxy (MakeInterval (Pitch F Natural Oct3) (Pitch B Natural Oct3))
 x = undefined
 
--- class ValidHarmInterval i
--- instance TypeError (Text "Can't have minor seconds in chords.") => ValidHarmInterval (Interval Aug Unison)
--- instance TypeError (Text "Can't have minor seconds in chords.") => ValidHarmInterval (Interval Min Second)
--- instance TypeError (Text "Can't have major sevenths in chords.") => ValidHarmInterval (Interval Maj Seventh)
--- instance TypeError (Text "Can't have major sevenths in chords.") => ValidHarmInterval (Interval Dim Octave)
--- instance {-# OVERLAPPABLE #-} ValidHarmInterval i
 
 
 type Invalid = True ~ False
 type Valid = (() :: Constraint)
 
-type family ValidHarmInterval i :: Bool where
-    ValidHarmInterval (Interval Aug Unison) = TypeError (Text "Can't have minor seconds in chords.")
-    ValidHarmInterval (Interval Min Second) = TypeError (Text "Can't have minor seconds in chords.")
-    ValidHarmInterval (Interval Maj Seventh) = TypeError (Text "Can't have major sevenths in chords.")
-    ValidHarmInterval (Interval Dim Octave) = TypeError (Text "Can't have major sevenths in chords.")
-    ValidHarmInterval i = True
+-- type family ValidHarmInterval i :: Bool where
+--     ValidHarmInterval (Interval Aug Unison) = TypeError (Text "Can't have minor seconds in chords.")
+--     ValidHarmInterval (Interval Min Second) = TypeError (Text "Can't have minor seconds in chords.")
+--     ValidHarmInterval (Interval Maj Seventh) = TypeError (Text "Can't have major sevenths in chords.")
+--     ValidHarmInterval (Interval Dim Octave) = TypeError (Text "Can't have major sevenths in chords.")
+--     ValidHarmInterval i = True
 
-data ValidTest where
-    V :: Ensure (ValidHarmInterval (MakeInterval (Note pc1 a1 o1 d1) (Note pc2 a2 o2 d2)))
-            => Note pc1 a1 o1 d1 -> Note pc2 a2 o2 d2 -> ValidTest
 
-as :: ValidTest
-as = V n1 n2
-
-n1 :: Note B Flat Oct3 Half
-n1 = undefined
-
-n2 :: Note C Natural Oct3 Half
-n2 = undefined
-
-y :: Proxy (OctSucc Oct8)
-y = undefined
 
 -- type family ValidPitch (p :: Pitch) :: Bool where
 --     ValidPitch ('Pitch pc acc o) = TypeError (Text "Octave too high.")
