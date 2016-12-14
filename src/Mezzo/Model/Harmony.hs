3fa9a1a4cf45abe9c523e63522de53d4dec1db17
Dima Szamozvancev <ds709@cam.ac.uk>
2016-12-14 20:35:47 +0100

    Start implementing Mezzo.Model.Harmony
    Currently only dealing with consonance and harmonic motion.

---
+++ src/Mezzo/Model/Harmony.hs
@@ -0,0 +1,91 @@
+{-# LANGUAGE  TypeInType, MultiParamTypeClasses, FlexibleInstances,
+    UndecidableInstances #-}
+
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Mezzo.Model.Harmony
+-- Description :  Mezzo harmonic model
+-- Copyright   :  (c) Dima Szamozvancev
+-- License     :  MIT
+--
+-- Maintainer  :  ds709@cam.ac.uk
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Types and type functions modelling classical harmony.
+--
+-----------------------------------------------------------------------------
+
+module Mezzo.Model.Harmony
+    (
+    -- * Consonant and dissonant intervals
+      PerfConsonantInterval
+    , ImperfConsonantInterval
+    , DissonantInterval
+    -- * Harmonic motion
+    , DirectMotion
+    , ContraryMotion
+    , ObliqueMotion
+    ) where
+
+import GHC.TypeLits
+
+import Mezzo.Model.Types
+
+-------------------------------------------------------------------------------
+-- Consonant and dissonant intervals
+-------------------------------------------------------------------------------
+
+-- | Classifies perfect consonant intervals:
+--
+--  * Perfect unisons
+--  * Perfect fifths
+--  * Perfect octaves
+class PerfConsonantInterval (i :: IntervalType)
+instance PerfConsonantInterval (Interval Perf Unison)
+instance PerfConsonantInterval (Interval Perf Fifth)
+instance PerfConsonantInterval (Interval Perf Octave)
+
+-- | Classifies imperfect consonant intervals:
+--
+--  * Major and minor thirds
+--  * Major and minor sixths
+class ImperfConsonantInterval (i :: IntervalType)
+instance ImperfConsonantInterval (Interval Maj Third)
+instance ImperfConsonantInterval (Interval Min Third)
+instance ImperfConsonantInterval (Interval Maj Sixth)
+instance ImperfConsonantInterval (Interval Min Sixth)
+
+-- | Classifies dissonant intervals:
+--
+--  * Perfect fourth (by common practice convention)
+--  * Augmented and diminished intervals
+--  * Second and seventh intervals
+class DissonantInterval (i :: IntervalType)
+instance DissonantInterval (Interval Perf Fourth)
+instance DissonantInterval (Interval Aug is)
+instance DissonantInterval (Interval Dim is)
+instance DissonantInterval (Interval ic Second)
+instance DissonantInterval (Interval ic Seventh)
+
+-------------------------------------------------------------------------------
+-- Harmonic motion
+-------------------------------------------------------------------------------
+
+-- | Ensures that direct motion is permitted between the two intervals.
+class DirectMotion (i1 :: IntervalType) (i2 :: IntervalType)
+instance {-# OVERLAPPING #-} TypeError (Text "Direct motion into a perfect unison is forbidden.")
+                                => DirectMotion i1 (Interval Perf Unison)
+instance {-# OVERLAPPING #-} TypeError (Text "Direct motion into a perfect fifth is forbidden.")
+                                => DirectMotion i1 (Interval Perf Fifth)
+instance {-# OVERLAPPING #-} TypeError (Text "Direct motion into a perfect octave is forbidden.")
+                                => DirectMotion i1 (Interval Perf Octave)
+instance {-# OVERLAPPABLE #-}      DirectMotion i1 i2
+
+-- | Ensures that contrary motion is permitted between the two intervals.
+class ContraryMotion (i1 :: IntervalType) (i2 :: IntervalType)
+instance ContraryMotion i1 i2
+
+-- | Ensures that oblique motion is permitted between the two intervals.
+class ObliqueMotion (i1 :: IntervalType) (i2 :: IntervalType)
+instance ObliqueMotion i1 i2
