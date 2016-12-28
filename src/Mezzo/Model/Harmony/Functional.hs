412b543de33c26c91979b9ad95a6726bd212230e
Dima Szamozvancev <ds709@cam.ac.uk>
2016-12-28 21:47:15 +0100

    Add basic functional grammar to Mezzo.Model.Harmony.Functional

---
+++ src/Mezzo/Model/Harmony/Functional.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE  TypeInType, MultiParamTypeClasses, FlexibleInstances,
+    UndecidableInstances, GADTs, TypeOperators, TypeFamilies #-}
+
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Mezzo.Model.Harmony.Functional
+-- Description :  Models of functional harmony
+-- Copyright   :  (c) Dima Szamozvancev
+-- License     :  MIT
+--
+-- Maintainer  :  ds709@cam.ac.uk
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Types and type functions modelling principles of functional harmony.
+--
+-----------------------------------------------------------------------------
+
+module Mezzo.Model.Harmony.Functional
+    (
+    ) where
+
+import GHC.TypeLits
+import Data.Kind
+
+import Mezzo.Model.Types hiding (IntervalClass (..))
+import Mezzo.Model.Prim
+
+data KeyType = Key PitchClass Accidental Mode
+data Mode = MajorMode | MinorMode
+
+data ScaleDegree = I | II | III | IV | V | VI | VII
+
+data Quality = Maj | Min | Dom7 | Dim
+
+
+data ScaleRoot (s :: ScaleDegree) (a :: Accidental) = Root
+data ScaleChord (d :: ScaleDegree) (a :: Accidental) (q :: Quality) = SC
+
+data Degree (sd :: ScaleDegree) (q :: Quality) where
+    DegChord :: ScaleChord d a q -> Degree d q
+
+data Piece (m :: Mode) = Piece [Phrase m]
+
+data Phrase (m :: Mode) where
+    PhraseIVI :: Tonic m -> Dominant m -> Tonic m -> Phrase m
+    PhraseVI ::             Dominant m -> Tonic m -> Phrase m
+
+data Tonic (m :: Mode) where
+    TonMaj :: Degree I Maj -> Tonic MajorMode
+    TonMin :: Degree I Min -> Tonic MinorMode
+
+data Dominant (m :: Mode) where
+    DomV7   ::                   Degree V Dom7  -> Dominant m
+    DomVM   ::                   Degree V Maj   -> Dominant m
+    DomVII0 ::                   Degree VII Dim -> Dominant m
+    DomSD   :: SubDominant m  -> Dominant m     -> Dominant m
+    DomSecD :: Degree II Dom7 -> Degree V Dom7  -> Dominant m
+
+data SubDominant (m :: Mode) where
+    SubIIm     ::                   Degree II Min -> SubDominant MajorMode
+    SubIVM     ::                   Degree IV Maj -> SubDominant MajorMode
+    SubIIImIVM :: Degree III Min -> Degree IV Maj -> SubDominant MajorMode
+    SubIVm     ::                   Degree IV Min -> SubDominant MinorMode
