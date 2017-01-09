7f9d6e9ef709f1425164423bb9a81c00dee481d1
Dima Szamozvancev <ds709@cam.ac.uk>
2017-01-09 19:32:43 +0100

    Reexport music model modules in Mezzo.Model

--- src/Mezzo/Model.hs
+++ src/Mezzo/Model.hs
@@ -1,6 +1,37 @@
-module Mezzo.Model
-    ( someFunc
-    ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Mezzo.Model
+-- Description :  Mezzo music model
+-- Copyright   :  (c) Dima Szamozvancev
+-- License     :  MIT
+--
+-- Maintainer  :  ds709@cam.ac.uk
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Module providing the external interface to the Mezzo type-level music model.
+--
+-----------------------------------------------------------------------------
+
+module Mezzo.Model (module X) where
+
+-- Uses import/export shortcut as suggested by HLint.
+
+import Mezzo.Model.Types as X
+    (
+      PitchClass (..)
+    , Accidental (..)
+    , OctaveNum (..)
+    , Duration (..)
+    , PC (..)
+    , Acc (..)
+    , Oct (..)
+    , Dur (..)
+    , PitchType (..)
+    , Pit (..)
+    , FromPitch
+    )
+import Mezzo.Model.Harmony.Chords as X
+import Mezzo.Model.Harmony.Functional as X
+import Mezzo.Model.Music as X
